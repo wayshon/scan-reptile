7e9ba4d45d34c3f85ecd38def0fb71930eaa82c4
wayshon <wayshon@qq.com>
2019-08-29 19:33:01 +0800

    demo ok

--- task/FoodDemo.js
+++ task/FoodDemo.js
@@ -1,103 +1,85 @@
 const cheerio = require('cheerio');
 // const FoodModel = require('../model/food');
-const http = require('http');
+const http = require('https');
 const url = require('url');
+const iconv = require('iconv-lite');
 
 /**
  * 定义请求地址
  * @type {*}
  */
 // 根路径
-const _Host = 'www.2717.com';
+const _Host = 'https://www.2717.com';
 // 食物
 const _Food = '/meishitupian/';
-// 绝对路径
-const _PATH = `https://${_Host}${_Food}`;
 
 /**
  * 发请求，获取首页数据
  */
 const fetchHome = async (urlStr) => {
-
-    http.get(`http://${_Host}${_Food}`,(res) => {
-        console.log(`状态码: ${res.statusCode}`);
-        res.setEncoding('hex');
-
-        let rawData = '';
-        res.on('data', (chunk) => { rawData += chunk; });
-        res.on('end', () => {
-            console.log(rawData)
-        });
-    }).on('error', (e) => {
-        console.error(`出现错误: ${e.message}`);
-    });
-
-
     // const urlObj = url.parse(urlStr);
-    // const { host, protocol, path } = urlObj;
-    // return new Promise((resolve, reject) => {
-    //     const options = {
-    //         hostname: host,
-    //         port: /https/.test(protocol) ? 443 : 80,
-    //         path: path,
-    //         method: 'GET',
-    //         timeout: 10000,
-    //         headers: {
-    //             'Referer': 'https://www.google.com',
-    //             'Accept': 'image/webp,image/*,*/*;q=0.8',
-    //             'Accept-Encoding': 'gzip, deflate',
-    //             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36',
-    //         }
-    //     };
+    // const { host, path } = urlObj;
+    // http
+    // .request({
+    //     url: urlStr,
+    //     hostname: host,
+    //     port: 443,
+    //     path: path,
+    //     method: 'GET',
+    //     headers: {
+    //         'Referer': 'https://www.google.com',
+    //         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36',
+    //     }
+    // }, (res) => {
+    //     console.log(`状态码: ${res.statusCode}`);
+    //     // res.setEncoding('hex');
 
-    //     const req = http.request(options, (res) => {
-    //         console.log(`状态码: ${res.statusCode}`);
-    //         console.log(res);
-    //         // if (res.headers.location) {
-    //         //     console.log(`重定向: ${res.headers.location}`);
-    //         //     fetchHome(res.headers.location);
-    //         //     return;
-    //         // }
-    //         // res.setEncoding('utf-8');
-    //         res.on('data', (chunk) => {
-    //             console.log(`响应主体: ${chunk}`);
-    //         });
-    //         res.on('end', () => {
-    //             console.log('响应中已无数据。');
-    //         });
+    //     let rawData = '';
+    //     res.on('data', (chunk) => { rawData += chunk; });
+    //     res.on('end', () => {
+    //         const buffer = Buffer.from(rawData);
+    //         const result = iconv.decode(buffer, 'gb2312');
+    //         console.log(result)
     //     });
+    // }).on('error', (e) => {
+    //     console.error(`出现错误: ${e.message}`);
+    // }).end();
 
-    //     req.on('error', (e) => {
-    //         console.error(`请求遇到问题: ${e.message}`);
-    //     });
-
-    //     req.end();
-    // });
+    return new Promise((resolve, reject) => {
+        http.get(urlStr, (res) => {
+            console.log(`状态码: ${res.statusCode}`);
+            // res.setEncoding('hex');
+    
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+            res.on('end', () => {
+                const buffer = Buffer.from(rawData);
+                const result = iconv.decode(buffer, 'gb2312');
+                resolve(result);
+            });
+        }).on('error', (e) => {
+            reject(e)
+        });
+    });
 };
 
 /**
  * 获取详情页面的数据
  */
 const fetchDetail = async (url) => {
-    // return new Promise((resolve, reject) => {
-    // 	superagent
-    // 		.get(`${_Host}${url}`)
-    // 		.set('Referer', 'https://www.google.com')
-    // 		.set('Accept', 'image/webp,image/*,*/*;q=0.8')
-    // 		.set('Accept-Encoding', 'gzip, deflate')
-    // 		.set(
-    // 			'User-Agent',
-    // 			'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36'
-    // 		)
-    // 		.charset('gbk')
-    // 		.end(function (err, res) {
-    // 			if (err) {
-    // 				reject(err);
-    // 			} else {
-    // 				resolve(res.text);
-    // 			}
-    // 		});
-    // });
+    return new Promise((resolve, reject) => {
+        http.get(`${_Host}${url}`, (res) => {
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+            res.on('end', () => {
+                const buffer = Buffer.from(rawData);
+                const result = iconv.decode(buffer, 'gb2312');
+                resolve(result);
+            });
+        }).on('error', (e) => {
+            reject(e)
+        });
+    });
 };
 
 /**
@@ -139,6 +121,7 @@ const parseImages = async (url) => {
  * 插入mongodb
  */
 const insertMongo = (data) => {
+    console.log('insert ================================= ', data);
     // let food = new FoodModel(data);
     // food.save((err, res) => {
     // 	if (err) {
@@ -157,76 +140,75 @@ const insertMongo = (data) => {
 const foodTask = async () => {
     let resText;
     try {
-        resText = await fetchHome(_PATH);
+        resText = await fetchHome(`${_Host}${_Food}`);
     } catch (e) {
         console.log(e);
         return;
     }
-    console.log(resText)
 
-    // const $ = cheerio.load(resText);
+    const $ = cheerio.load(resText);
 
-    // /**
-    //  * 遍历首页的内容数组，调用mongo存储
-    //  * @param {Documents li} lis 
-    //  */
-    // const insert = async (lis) => {
-    // 	lis.each((i, elem) => {
-    // 		const _this = $(elem);
-    // 		let item = {
-    // 			title: _this.find('a.tit').attr('title'),
-    // 			img: _this.find('img').attr('src'),
-    // 			images: []
-    // 		}
+    /**
+     * 遍历首页的内容数组，调用mongo存储
+     * @param {Documents li} lis 
+     */
+    const insert = async (lis) => {
+    	lis.each((i, elem) => {
+    		const _this = $(elem);
+    		let item = {
+    			title: _this.find('a.tit').attr('title'),
+    			img: _this.find('img').attr('src'),
+    			images: []
+    		}
 
-    // 		const href = _this.find('a').attr('href');
-    // 		if (href) {
-    // 			parseImages(href).then(images => item.images = images).catch(e => console.log(e)).then(() => insertMongo(item));
-    // 		} else {
-    // 			insertMongo(item);
-    // 		}
-    // 	});
-    // };
+    		const href = _this.find('a').attr('href');
+    		if (href) {
+    			parseImages(href).then(images => item.images = images).catch(e => console.log(e)).then(() => insertMongo(item));
+    		} else {
+    			insertMongo(item);
+    		}
+    	});
+    };
 
-    // insert($('div.MeinvTuPianBox li'));
+    insert($('div.MeinvTuPianBox li'));
 
-    // const parseNext = async (url) => {
-    // 	let resText;
-    // 	try {
-    // 		resText = await fetchHome(url);
-    // 	} catch (e) {
-    // 		console.log(e);
-    // 		return;
-    // 	}
+    const parseNext = async (url) => {
+    	let resText;
+    	try {
+    		resText = await fetchHome(`${_Host}${url}`);
+    	} catch (e) {
+    		console.log(e);
+    		return;
+    	}
 
-    // 	insert($('div.MeinvTuPianBox li'));
-    // };
+    	insert($('div.MeinvTuPianBox li'));
+    };
 
-    // const last = $('div.NewPages a').last();
-    // const lastName = last.text();
-    // if (lastName === '末页') {
-    // 	const lastHref = last.attr('href');
-    // 	let allPages, path;
-    // 	try {
-    // 		let arr = lastHref.split('_');
-    // 		allPages = +arr.pop().split('.').shift();
-    // 		path = arr.join('_');
-    // 	} catch (e) {
-    // 		console.log(e);
-    // 	}
-    // 	if (allPages && path) {
-    // 		let i = 2;
-    // 		while (i < allPages) {
-    // 			let url = `${_Food}${path}_${i}.html`;
-    // 			try {
-    // 				await parseNext(url);
-    // 			} catch (e) {
-    // 				console.log(e)
-    // 			}
-    // 			i++;
-    // 		}
-    // 	}
-    // }
+    const last = $('div.NewPages a').last();
+    const lastName = last.text();
+    if (lastName === '末页') {
+    	const lastHref = last.attr('href');
+    	let allPages, path;
+    	try {
+    		let arr = lastHref.split('_');
+    		allPages = +arr.pop().split('.').shift();
+    		path = arr.join('_');
+    	} catch (e) {
+    		console.log(e);
+    	}
+    	if (allPages && path) {
+    		let i = 2;
+    		while (i < allPages) {
+    			let url = `${_Food}${path}_${i}.html`;
+    			try {
+    				await parseNext(url);
+    			} catch (e) {
+    				console.log(e)
+    			}
+    			i++;
+    		}
+    	}
+    }
 };
 
 module.exports = foodTask;
